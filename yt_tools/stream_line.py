7bed39f2dd9f2de909487f51816077e7f92dd22c
陈国银 <106894282+gychen-NJU@users.noreply.github.com>
2024-09-10 15:32:30 +0800

    Update stream_line.py
    add the trilinear_interpolation function
    update the field_line intergrator from the near interpolation to trilinear interpolation

--- yt_tools/stream_line.py
+++ yt_tools/stream_line.py
@@ -20,6 +20,20 @@ def sphere_sample(point, r=1, nsample=10):
     ret = np.vstack([xlist,ylist,zlist]).T
     return ret
 
+def trilinear_interpolation(field, idx):
+    xi,yi,zi    = np.floor(idx).astype(int).T if len(np.shape(idx))>1 else np.floor(idx).astype(int)
+    xd,yd,zd    = (idx-np.floor(idx)).T
+    nx,ny,nz    = field.shape[:3]
+    xf,yf,zf    = np.where(xi+1>nx-1,nx-1,xi+1), np.where(yi+1>ny-1,ny-1,yi+1), np.where(zi+1>nz-1,nz-1,zi+1)
+    f           = field
+    xd              = xd[(slice(None),)+(np.newaxis,)*(f[xi,yi,zi].ndim-1)]
+    yd              = yd[(slice(None),)+(np.newaxis,)*(f[xi,yi,zi].ndim-1)]
+    zd              = zd[(slice(None),)+(np.newaxis,)*(f[xi,yi,zi].ndim-1)]
+    ret         = f[xi,yi,zi]*(1-xd)*(1-yd)*(1-zd)+f[xf,yi,zi]*xd*(1-yd)*(1-zd)+f[xi,yf,zi]*(1-xd)*yd*(1-zd)+\
+                  f[xi,yi,zf]*(1-xd)*(1-yd)*zd+f[xf,yf,zi]*xd*yd*(1-zd)+f[xf,yi,zf]*xd*(1-yd)*zd+\
+                  f[xi,yf,zf]*(1-xd)*yd*zd+f[xf,yf,zf]*xd*yd*zd
+    return ret
+
 def field_line(field, star_point, ds=1.0, max_step=10000, dxyz=[1,1,1]):
     dxyz_min_idx=np.argmin(dxyz)
     Dx,Dy,Dz=np.array(dxyz)/dxyz[dxyz_min_idx]
@@ -32,31 +46,29 @@ def field_line(field, star_point, ds=1.0, max_step=10000, dxyz=[1,1,1]):
     iters = 0
     lb = np.array([0,0,0])
     ub = np.array(field.shape[-3:])-1
+    over_max_step = False
     
     while True:
-        xi, yi, zi = np.around(np.array([x0, y0, z0])).astype(int)
-        bvec = np.array(field[:,xi,yi,zi])
+        xi, yi, zi = x0, y0, z0
+        bvec = trilinear_interpolation(field.transpose(1,2,3,0), np.array([[xi,yi,zi]]))[0]
         bb = np.sum(bvec**2)**0.5
         if bb<1e-10:
-            # print('encounter the boundary')
             break
         k1 = bvec/bb
-        xi, yi, zi = np.around(np.array(np.array([x0,y0,z0])+k1*ds/2*scale)).astype(int)
-        bvec = np.array(field[:,xi,yi,zi])
+        xi, yi, zi = np.array([x0,y0,z0])+k1*ds/2*scale
+        bvec = trilinear_interpolation(field.transpose(1,2,3,0), np.array([[xi,yi,zi]]))[0]
         bb = np.sum(bvec**2)**0.5
         if bb<1e-10:
-            # print('encounter the boundary')
             break
         k2 = bvec/bb
-        xi, yi, zi = np.around((np.array(np.array([x0,y0,z0])+k2*ds/2*scale))).astype(int)
-        bvec = np.array(field[:,xi,yi,zi])
+        xi, yi, zi = np.array([x0,y0,z0])+k2*ds/2*scale
+        bvec = trilinear_interpolation(field.transpose(1,2,3,0), np.array([[xi,yi,zi]]))[0]
         bb = np.sum(bvec**2)**0.5
         if bb<1e-10:
-            # print('encounter the boundary')
             break
         k3 = bvec/bb
-        xi, yi, zi = np.around(np.array(np.array([x0,y0,z0])+k3*ds*scale)).astype(int)
-        bvec = np.array(field[:,xi,yi,zi])
+        xi, yi, zi = np.array([x0,y0,z0])+k3*ds*scale
+        bvec = trilinear_interpolation(field.transpose(1,2,3,0), np.array([[xi,yi,zi]]))[0]
         bb = np.sum(bvec**2)**0.5
         if bb<1e-10:
             # print('encounter the boundary')
@@ -65,8 +77,8 @@ def field_line(field, star_point, ds=1.0, max_step=10000, dxyz=[1,1,1]):
         x0, y0, z0 = np.array([x0,y0,z0])+(k1+2*k2+2*k3+k4)*ds/6.*scale
         iters += 1
         if x0<lb[0] or x0>ub[0] or y0<lb[1] or y0>ub[1] or z0<lb[2] or z0>ub[2] or iters>=max_step:
-            if iters >= max_step:
-                print('over the max step: %05d during the forward integrating' % iters)
+            # if iters >= max_step:
+            #     print('over the max step: %05d during the forward integrating' % iters)
             break
         fieldline1.append(np.array([x0, y0, z0]))
         
@@ -74,39 +86,39 @@ def field_line(field, star_point, ds=1.0, max_step=10000, dxyz=[1,1,1]):
     x0, y0, z0 = star_point
     iters = 0
     while True:
-        xi, yi, zi = np.around(np.array([x0, y0, z0])).astype(int)
-        bvec = np.array(field[:,xi,yi,zi])
+        xi, yi, zi = x0,y0,z0
+        bvec = trilinear_interpolation(field.transpose(1,2,3,0), np.array([[xi,yi,zi]]))[0]
         bb = np.sum(bvec**2)**0.5
         if bb<1e-10:
             # print('encounter the boundary')
             break
         k1 = -bvec/bb
-        xi, yi, zi = np.around(np.array(np.array([x0,y0,z0])+k1*ds/2)).astype(int)
-        bvec = np.array(field[:,xi,yi,zi])
+        xi, yi, zi = np.array([x0,y0,z0])+k1*ds/2*scale
+        bvec = trilinear_interpolation(field.transpose(1,2,3,0), np.array([[xi,yi,zi]]))[0]
         bb = np.sum(bvec**2)**0.5
         if bb<1e-10:
             # print('encounter the boundary')
             break
         k2 = -bvec/bb
-        xi, yi, zi = np.around(np.array(np.array([x0,y0,z0])+k2*ds/2)).astype(int)
-        bvec = np.array(field[:,xi,yi,zi])
+        xi, yi, zi = np.array([x0,y0,z0])+k2*ds/2*scale
+        bvec = trilinear_interpolation(field.transpose(1,2,3,0), np.array([[xi,yi,zi]]))[0]
         bb = np.sum(bvec**2)**0.5
         if bb<1e-10:
             # print('encounter the boundary')
             break
         k3 = -bvec/bb
-        xi, yi, zi = np.around(np.array(np.array([x0,y0,z0])+k3*ds)).astype(int)
-        bvec = np.array(field[:,xi,yi,zi])
+        xi, yi, zi = np.array([x0,y0,z0])+k3*ds*scale
+        bvec = trilinear_interpolation(field.transpose(1,2,3,0), np.array([[xi,yi,zi]]))[0]
         bb = np.sum(bvec**2)**0.5
         if bb<1e-10:
             # print('encounter the boundary')
             break
         k4 = -bvec/bb
-        x0, y0, z0 = np.array([x0,y0,z0])+(k1+2*k2+2*k3+k4)*ds/6.
+        x0, y0, z0 = np.array([x0,y0,z0])+(k1+2*k2+2*k3+k4)*ds/6.*scale
         iters += 1
         if x0<lb[0] or x0>ub[0] or y0<lb[1] or y0>ub[1] or z0<lb[2] or z0>ub[2] or iters>=max_step:
-            if iters >= max_step:
-                print('over the max step: %05d during the backward integrating' % iters)
+            # if iters >= max_step:
+            #     print('over the max step: %05d during the backward integrating' % iters)
             break
         fieldline2.append(np.array([x0, y0, z0]))
         
